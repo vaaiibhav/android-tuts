33d442089f614dec4c2b003dc24bc0a1c5447b1c
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-18 10:47:50 +0530

    added WebView
    added Webview and Loaded URL google,com

--- Notification/app/src/main/java/com/example/notification/MainActivity.java
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.notification;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        wv = (WebView) findViewById(R.id.webview_id);
+        wv.loadUrl("https://www.google.com/");
+            }
 }
