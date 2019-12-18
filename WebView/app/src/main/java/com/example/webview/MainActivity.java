c2ab45df9271742d976cfbd5b843babe69d629e8
Vaaiibhav Vaidya <vaaiibhav@live.com>
2019-12-18 11:59:39 +0530

    final output

--- WebView/app/src/main/java/com/example/webview/MainActivity.java
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView wv = findViewById(R.id.)
     }
 }
