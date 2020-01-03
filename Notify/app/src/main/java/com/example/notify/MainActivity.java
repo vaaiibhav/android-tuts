eb7aa1d54201c35427e12585a7b136368a257ff0
Vaaiibhav Vaidya <vaaiibhav@live.com>
2020-01-03 10:27:04 +0530

    commited everythin from last year

--- Notify/app/src/main/java/com/example/notify/MainActivity.java
+++ Notify/app/src/main/java/com/example/notify/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String firebasemessage ="";
+        String firebasemessage ="New Message";
 
-       showNotify(firebasemessage);
+        showNotify(firebasemessage);
 
     }
 
@@ -28,7 +30,18 @@ public class MainActivity extends AppCompatActivity {
         notBuilder.setSmallIcon(R.drawable.ic_launcher_background);
         notBuilder.setContentText("This is some Sampple Text");
 
+        Intent notIntent = new Intent(MainActivity.this, ShowScreenActivity.class);
+        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pi =  PendingIntent.getActivity(this,0,notIntent,PendingIntent.FLAG_UPDATE_CURRENT );
+        notBuilder.setContentIntent(pi);
+
+
 
         NotificationManager mgr= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mgr.notify(0,notBuilder.build());}
+        mgr.notify(0,notBuilder.build());
+
+
+
+    }
 }
